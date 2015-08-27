3197229118f0f94b337b00895ce5ab834ed78517
Michael Paul <michael@redfuel.com.br>
2015-08-27 11:34:37 -0300

    Algoritmo para tratar os operadores lógicos

---
+++ projects/07/eq.asm
@@ -0,0 +1,21 @@
+    @x
+    M=1
+    @y
+    M=-1
+
+    @x
+    D=M
+    @y
+    D=D-M // x - y
+    @FALSE
+    D;JNE // if ($x - $y != 0) goto f;
+    @r
+    M=-1 // $r = true;
+    @END
+    0;JMP // goto end;
+(FALSE) // f:
+    @r
+    M=0 // $r = false;
+(END) // end:
+    @r
+    D=M // return $r;
