c236d7179f4d9b4faa443f27535a23595b7f8a59
Michael Paul <michael@michaelpaul.com.br>
2017-01-18 23:10:39 -0200

    Fix: operador duplicado por falta de parenteses

--- projects/10-11/bin/JackCompiler.php
+++ projects/10-11/bin/JackCompiler.php
@@ -8,16 +8,26 @@ require 'vendor/autoload.php';
 use JackCompiler\Main;
 
 if (in_array('-h', $argv) || in_array('--help', $argv)) {
-    echo("usage:  JackCompiler.php\t\t - compiles all .jack files in current directory" . PHP_EOL);
-    echo("\tJackCompiler.php Directory\t - compiles all .jack in Directory" . PHP_EOL);
-    echo("\tJackCompiler.php Source.jack\t - compiles Source.jack to Source.vm" . PHP_EOL);
+    echo("usage:  JackCompiler.php [--ast]\t\t - compiles all .jack files in current directory" . PHP_EOL);
+    echo("\tJackCompiler.php [--ast] Directory\t - compiles all .jack in Directory" . PHP_EOL);
+    echo("\tJackCompiler.php [--ast] Source.jack\t - compiles Source.jack to Source.vm" . PHP_EOL);
 } else {
-    $input = $argv[1];
-    $dumpAst = false;
-    if ('--ast' == $input) {
-        $dumpAst = true;
+    $input = null;
+    $ast = false;
+    
+    if ($argc == 2) {
+        if ($argv[1] == '--ast') {
+            $ast = true;
+        } else {
+            $input = $argv[1];
+        }
+    } else if ($argc == 3) {
+        if ($argv[1] == '--ast') {
+            $ast = true;
+        }
         $input = $argv[2];
     }
+
     $compiler = new Main($input);
-    $compiler->compile($dumpAst);
+    $compiler->compile($ast);
 }
